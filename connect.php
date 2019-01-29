5feebe92614a282067d4253db4c35b8c547206b6
Vladislav Terenchuk <asdof71@yandex.ru>
2019-01-29 14:40:27 +0300

    Edit readme.md

--- connect.php
+++ connect.php
@@ -1,8 +1,4 @@
 <?php
-     ini_set('display_errors', 1);
-     ini_set('display_startup_errors', 1);
-     error_reporting(E_ALL);
-
      // Использовал PDO, потому что разрабатывал на PHP 7.2, а там mysql_connect удален.
 
      $host = "localhost"; // Имя сервера
